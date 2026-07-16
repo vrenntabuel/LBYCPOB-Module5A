5be10dd7b38c3aa248714c1a2f3629d494a365ab
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-07-16 12:54:21 +0800

    Added Hangman Project

---
+++ HangmanSpringBoot/src/main/java/ph/edu/dlsu/lbycpob/hangman/HangmanSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.hangman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangmanSpringBootApplication.class, args);
+    }
+
+}
---
+++ HangmanSpringBoot/src/test/java/ph/edu/dlsu/lbycpob/hangman/HangmanSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.hangman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
