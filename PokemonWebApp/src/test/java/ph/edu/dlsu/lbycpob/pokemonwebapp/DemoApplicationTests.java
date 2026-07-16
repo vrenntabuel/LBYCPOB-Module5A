e99f7aea40b1fe2b1f94c5856ea135c4aa2a30fb
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-07-16 13:03:44 +0800

    Add files via upload
    Added Pokemon Web App Project

---
+++ PokemonWebApp/src/test/java/ph/edu/dlsu/lbycpob/pokemonwebapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.pokemonwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
