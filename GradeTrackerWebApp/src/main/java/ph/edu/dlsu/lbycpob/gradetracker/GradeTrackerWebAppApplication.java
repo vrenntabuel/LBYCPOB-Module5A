be9c7234ff2cfded8c6c60a6cb178bdcf9ca7b7d
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-07-16 12:47:02 +0800

    Added Grade Tracker project

---
+++ GradeTrackerWebApp/src/main/java/ph/edu/dlsu/lbycpob/gradetracker/GradeTrackerWebAppApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.gradetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeTrackerWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradeTrackerWebAppApplication.class, args);
+    }
+
+}
---
+++ GradeTrackerWebApp/src/test/java/ph/edu/dlsu/lbycpob/gradetracker/GradeTrackerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.gradetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeTrackerWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
