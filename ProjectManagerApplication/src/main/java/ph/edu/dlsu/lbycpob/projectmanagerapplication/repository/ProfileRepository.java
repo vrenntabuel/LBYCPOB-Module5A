82de8d2a313bda56c84d3290d8f062e61e7d47a6
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-07-20 12:40:11 +0800

    ProjectManager-Added ProfileRepository.java code in the program

--- ProjectManagerApplication/src/main/java/ph/edu/dlsu/lbycpob/projectmanagerapplication/repository/ProfileRepository.java
+++ ProjectManagerApplication/src/main/java/ph/edu/dlsu/lbycpob/projectmanagerapplication/repository/ProfileRepository.java
@@ -1,4 +1,19 @@
-package ph.edu.dlsu.lbycpob.projectmanagerapplication.repository;
+package ph.edu.dlsu.lbycpob.profilemanager.repository;
 
-public interface ProfileRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<Profile, UUID> {
+
+    List<Profile> findAllByOrderByNameAsc();
+
+    Optional<Profile> findByNameIgnoreCase(String name);
+
+    // Replaces the original app.js .ilike("name", `%${query}%`).limit(1) search
+    List<Profile> findByNameContainingIgnoreCaseOrderByNameAsc(String query);
 }
