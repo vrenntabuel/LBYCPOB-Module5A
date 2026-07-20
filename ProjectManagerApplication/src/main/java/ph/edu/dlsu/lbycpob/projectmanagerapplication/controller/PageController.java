683f6e4240d72dda5dd4cd6f857215be29a60669
Vrennt Abuel <vrennt_abuel@dlsu.edu.ph>
2026-07-20 12:33:32 +0800

    ProjectManager-Added PageController.java code in the program

--- ProjectManagerApplication/src/main/java/ph/edu/dlsu/lbycpob/projectmanagerapplication/controller/PageController.java
+++ ProjectManagerApplication/src/main/java/ph/edu/dlsu/lbycpob/projectmanagerapplication/controller/PageController.java
@@ -1,4 +1,13 @@
-package ph.edu.dlsu.lbycpob.projectmanagerapplication.controller;
+package ph.edu.dlsu.lbycpob.profilemanager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 }
